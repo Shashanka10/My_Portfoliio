95c4d97ba8282b510602ed52f7645681af0c106d
Shashanka <71580178+Shashanka10@users.noreply.github.com>
2022-09-29 16:18:03 +0545

    Hover effect over each navclick (use of useState => onClick)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,15 +5,17 @@ import {FaUserTie} from 'react-icons/fa';
 import {GiBookmarklet} from 'react-icons/gi';
 import {RiCustomerService2Fill} from 'react-icons/ri';
 import {MdContactPhone} from 'react-icons/md';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [activeNav,setactiveNav]=useState('#');
   return (
     <nav>
-      <a href='#'><FaHome /></a>
-      <a href='#About'><FaUserTie /></a>
-      <a href='#Experience'><GiBookmarklet /></a>
-      <a href='#Services'><RiCustomerService2Fill /></a>
-      <a href='#Contact'><MdContactPhone /></a>
+      <a href='#' onClick={()=> setactiveNav('#')} className={activeNav === '#' ? 'active' : '' }><FaHome /></a>
+      <a href='#About' onClick={()=> setactiveNav('#About')} className={activeNav === '#About' ? 'active' : '' }><FaUserTie /></a>
+      <a href='#Experience' onClick={()=> setactiveNav('#Experience')} className={activeNav === '#Experience' ? 'active' : '' }><GiBookmarklet /></a>
+      <a href='#Services' onClick={()=> setactiveNav('#Services')} className={activeNav === '#Services' ? 'active' : '' }><RiCustomerService2Fill /></a>
+      <a href='#Contact' onClick={()=> setactiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : '' }><MdContactPhone /></a>
     </nav>
   )
 }
